3d50eb291a0f429171ce9c430a9732ced10b0ae1
qzchenwl <qzchenwl@gmail.com>
2023-04-29 23:00:29 +0800

    Initial Commit

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="streamchat-bot",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="qzchenwl",
+    author_email="qzchenwl@gmail.com",
     description="A simple Python package for creating Stream Chat bots.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/streamchat-bot",
+    url="https://github.com/qzchenwl/streamchat-bot",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
